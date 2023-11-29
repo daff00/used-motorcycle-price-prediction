8776e0253d16b5b8ada3978806ec477ac7d7f6f9
daff00 <daffa.kaisha@student.umn.ac.id>
2023-11-30 00:00:04 +0700

    Flask (masih error wkwk)

--- app.py
+++ app.py
@@ -4,18 +4,39 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pickle
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
+le = LabelEncoder()
+ss = StandardScaler()
+
+@app.route('/')
+def index():
+    # Redirect to the home page
+    return redirect(url_for('home'))
+
+@app.route("/home")
 def home():
     return render_template("base.html")
+
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1, -1)
+    loaded_model = pickle.load(open("nn_model.pkl", "rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
 
 @app.route('/result', methods = ['POST'])
 def result():
     if request.method == 'POST':
         to_predict_list = request.form.to_dict()
-    return render_template("base.html")
+        json_str = json.dumps(to_predict_list)
+        #to_predict_list = list(to_predict_list.values())
+        # result = ValuePredictor(to_predict_list)      
+    # return render_template("base.html", result = result)
+    return json_str
 
 if __name__ == "__main__":
     app.run(debug=True)
